80bc18bfaaf53b4d1c5fce21aed8218d7313c3ed
Agrim <agrimsharma1714@gmail.com>
2024-02-08 22:30:16 +0530

    resolved the error

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const {replace} = useRouter();
-    if(term) {
+    if (term) {
       params.set('query', term);
-    } else{
+    } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
